a6c253acf676f7cea9e7c7364155bf10064ea634
NellySayon <nelly.sayonara@proton.me>
2024-02-08 13:54:11 +0100

    updated to latest rdt and added interaction with walletApi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { RadixDappToolkit } from "@radixdlt/radix-dapp-toolkit";
+import { useEffect, useState } from "react";
+import { RadixDappToolkit, DataRequestBuilder, createLogger } from "@radixdlt/radix-dapp-toolkit";
 
-function App() {
-  const rdt = RadixDappToolkit({
-    dAppDefinitionAddress:
-      "account_tdx_2_12xf3455ks96n777zvsw8a5hvuk2cqzm58ktwrfuzrf6yqrl6ydv2h6", // change this to your own DApp address 
-    networkId: 2, // Stokenet
-  });
+function App() {  
+  const [connectedAccount, setConnectedAccount] = useState("");
+
+  useEffect(() => {
+    const radixDappToolkit = RadixDappToolkit({
+      dAppDefinitionAddress:
+        "account_tdx_2_12xf3455ks96n777zvsw8a5hvuk2cqzm58ktwrfuzrf6yqrl6ydv2h6", // change this to your own DApp address
+      networkId: 2, // Stokenet
+    //  logger: createLogger(1),
+    });
+
+    radixDappToolkit.buttonApi.setTheme("white");
+
+    radixDappToolkit.walletApi.setRequestData(DataRequestBuilder.accounts().exactly(1));
+
+    radixDappToolkit.walletApi.walletData$.subscribe((walletData) => {
+     //  console.log("walletData in Layout: ", walletData);
+      if (walletData.accounts.length === 0) {
+        setConnectedAccount("unconnected");
+      } else {
+        setConnectedAccount(walletData.accounts[0].address);
+      }
+    });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is a simple create-react-app with Radix Dapp Toolkit using the
           Radix Connect Button.
         </p>
         <radix-connect-button />
-      </header>
+        <p>Your account is {connectedAccount}</p>
+      </header>     
     </div>
   );
 }
